456e447f15974c2c3b54744b8cfc90e5b920a4df
tyler <tyler@cruftbusters.com>
2022-03-08 17:47:19 -0600

    create and display linkage

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,32 @@
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+interface Zone {}
+
 export default function App() {
-  return <>hello world</>
+  const [[id, zone], setIdAndZone] = useState<[string, Zone]>(['', {}])
+  return (
+    <div style={{ height: '100%' }}>
+      <div>linkage</div>
+      {id ? (
+        `${id}: ${JSON.stringify(zone)}`
+      ) : (
+        <button
+          onClick={async () => {
+            const id = uuidv4()
+            const response = await fetch(
+              `https://linkage-api.cruftbusters.com/v1/zones/${id}`,
+              {
+                method: 'POST',
+              },
+            )
+            const zone = await response.json()
+            setIdAndZone([id, zone])
+          }}
+        >
+          create your linkage
+        </button>
+      )}
+    </div>
+  )
 }
